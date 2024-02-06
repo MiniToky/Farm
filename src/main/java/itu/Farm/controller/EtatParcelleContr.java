ae37e8752c6c1c5f2230f014305b2e6884beae19
MiniToky <rtokyandria@gmail.com>
2024-02-06 16:45:18 +0300

    Rectification Etat

--- src/main/java/itu/Farm/controller/EtatParcelleContr.java
+++ src/main/java/itu/Farm/controller/EtatParcelleContr.java
@@ -12,7 +12,7 @@ import java.sql.Timestamp;
 import java.util.List;
 
 @RestController
-@RequestMapping("/etqt")
+@RequestMapping("/etat")
 public class EtatParcelleContr {
     @Autowired
     EtatParcelleServ serv;
